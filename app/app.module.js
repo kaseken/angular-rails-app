71db219b44a3e8eebcfb462bd833c93209a5088e
kaseken <k.kaseda0925@gmail.com>
2019-09-28 17:42:40 +0900

    add proposal list component

--- app/app.module.js
+++ app/app.module.js
@@ -11,6 +11,7 @@ var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
 var homepage_component_1 = require("./homepage/homepage.component");
 var documents_component_1 = require("./documents/documents.component");
+var proposal_list_component_1 = require("./proposal/proposal-list.component");
 var app_routing_module_1 = require("./app.routing.module");
 var AppModule = /** @class */ (function () {
     function AppModule() {
@@ -18,7 +19,12 @@ var AppModule = /** @class */ (function () {
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule, app_routing_module_1.AppRoutingModule],
-            declarations: [app_component_1.AppComponent, homepage_component_1.HomepageComponent, documents_component_1.DocumentsComponent],
+            declarations: [
+                app_component_1.AppComponent,
+                homepage_component_1.HomepageComponent,
+                documents_component_1.DocumentsComponent,
+                proposal_list_component_1.ProposalListComponent
+            ],
             bootstrap: [app_component_1.AppComponent]
         })
     ], AppModule);
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { DocumentsComponent } from "./documents/documents.component";
+import { ProposalListComponent } from "./proposal/proposal-list.component";
 
 import { AppRoutingModule } from "./app.routing.module";
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule],
-  declarations: [AppComponent, HomepageComponent, DocumentsComponent],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    DocumentsComponent,
+    ProposalListComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
