04c27379e071a7e5347514b0468a030e8f6930ba
kaseken <k.kaseda0925@gmail.com>
2019-09-28 19:48:21 +0900

    add view for proposal list

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,8 +1,46 @@
 import { Component } from "@angular/core";
+import { Proposal } from "./proposal";
 
 @Component({
   moduleId: module.id,
   selector: "proposal-list",
   templateUrl: "proposal-list.component.html"
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(
+    15,
+    "ABC company",
+    "http://portfolio.com",
+    "Ruby on Rails",
+    150,
+    120,
+    15,
+    "example@email.com"
+  );
+  proposalTwo: Proposal = new Proposal(
+    15,
+    "ABC company",
+    "http://portfolio.com",
+    "Ruby on Rails",
+    150,
+    120,
+    15,
+    "example@email.com"
+  );
+  proposalThree: Proposal = new Proposal(
+    15,
+    "ABC company",
+    "http://portfolio.com",
+    "Ruby on Rails",
+    150,
+    120,
+    15,
+    "example@email.com"
+  );
+
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ];
+}
