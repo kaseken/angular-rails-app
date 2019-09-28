7e77bbb0eacd7309446afe8c2189e90625ce2e76
kaseken <k.kaseda0925@gmail.com>
2019-09-28 16:31:00 +0900

    add documents component

--- app/app.module.js
+++ app/app.module.js
@@ -10,6 +10,7 @@ var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
 var homepage_component_1 = require("./homepage/homepage.component");
+var documents_component_1 = require("./documents/documents.component");
 var app_routing_module_1 = require("./app.routing.module");
 var AppModule = /** @class */ (function () {
     function AppModule() {
@@ -17,7 +18,7 @@ var AppModule = /** @class */ (function () {
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule, app_routing_module_1.AppRoutingModule],
-            declarations: [app_component_1.AppComponent, homepage_component_1.HomepageComponent],
+            declarations: [app_component_1.AppComponent, homepage_component_1.HomepageComponent, documents_component_1.DocumentsComponent],
             bootstrap: [app_component_1.AppComponent]
         })
     ], AppModule);
