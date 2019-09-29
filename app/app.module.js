071dfa67cd6b1401e7930e41c7fb16d0126bfcbf
kaseken <k.kaseda0925@gmail.com>
2019-09-29 16:24:51 +0900

    add document service for connection with rails API

--- app/app.module.js
+++ app/app.module.js
@@ -9,10 +9,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
+var http_1 = require("@angular/http");
 var ng_bootstrap_1 = require("@ng-bootstrap/ng-bootstrap");
 var app_component_1 = require("./app.component");
 var homepage_component_1 = require("./homepage/homepage.component");
 var documents_component_1 = require("./documents/documents.component");
+var document_service_1 = require("./documents/document.service");
 var proposal_list_component_1 = require("./proposal/proposal-list.component");
 var proposal_new_component_1 = require("./proposal/proposal-new.component");
 var proposal_show_component_1 = require("./proposal/proposal-show.component");
@@ -22,7 +24,13 @@ var AppModule = /** @class */ (function () {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, app_routing_module_1.AppRoutingModule, forms_1.FormsModule, ng_bootstrap_1.NgbModule.forRoot()],
+            imports: [
+                platform_browser_1.BrowserModule,
+                app_routing_module_1.AppRoutingModule,
+                forms_1.FormsModule,
+                ng_bootstrap_1.NgbModule.forRoot(),
+                http_1.HttpModule
+            ],
             declarations: [
                 app_component_1.AppComponent,
                 homepage_component_1.HomepageComponent,
@@ -31,6 +39,7 @@ var AppModule = /** @class */ (function () {
                 proposal_new_component_1.ProposalNewComponent,
                 proposal_show_component_1.ProposalShowComponent
             ],
+            providers: [document_service_1.DocumentService],
             bootstrap: [app_component_1.AppComponent]
         })
     ], AppModule);
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { DocumentsComponent } from "./documents/documents.component";
+import { DocumentService } from "./documents/document.service";
 import { ProposalListComponent } from "./proposal/proposal-list.component";
 import { ProposalNewComponent } from "./proposal/proposal-new.component";
 import { ProposalShowComponent } from "./proposal/proposal-show.component";
@@ -13,7 +15,13 @@ import { ProposalShowComponent } from "./proposal/proposal-show.component";
 import { AppRoutingModule } from "./app.routing.module";
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule.forRoot()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    HttpModule
+  ],
   declarations: [
     AppComponent,
     HomepageComponent,
@@ -22,6 +30,7 @@ import { AppRoutingModule } from "./app.routing.module";
     ProposalNewComponent,
     ProposalShowComponent
   ],
+  providers: [DocumentService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
